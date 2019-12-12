21aca4237417200bef368ace9ddc61f6f729a5de
muhwid <muhwid29@gmail.com>
2019-12-12 19:36:05 +0800

    migrate to android x

---
+++ app/src/main/java/id/lima/baseapp/interfaces/DialogClickListener.java
@@ -0,0 +1,11 @@
+package id.lima.baseapp.interfaces;
+
+/**
+ * Created with love by muhwid on 2019-07-17.
+ * email muhwid29@gmail.com
+ */
+public interface DialogClickListener {
+    void onClickYes();
+
+    void onClickNo();
+}
